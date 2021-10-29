ec2edc824e94ee818daaa60966976e0bf2106206
xzsfaf15618a <92630434+xzsfaf15618a@users.noreply.github.com>
2021-10-29 20:05:09 +0800

    Add files via upload

---
+++ cs.js
@@ -0,0 +1,22 @@
+﻿<script type="text/javascript">
+var randoms = {
+	ads_codes: ['document.writeln("<script type=\\\'text/javascript\\\' src=\\\'https://cdn.jsdelivr.net/gh/xzsfaf15618a/adress/today.js\\\'><'+'/script>");','document.writeln("<script type=\\\'text/javascript\\\' src=\\\'https://mm3.mylmad.com/ad/content/120\\\'><'+'/script>");'],
+	ads_weight: [10,10],
+
+	get_random: function(weight) {
+		var s = eval(weight.join('+'));
+		var r = Math.floor(Math.random() * s);
+		var w = 0;
+		var n = weight.length - 1;
+		for(var k in weight){w+=weight[k];if(w>=r){n=k;break;}};
+		return n;
+	},
+	init: function() {
+
+		var rand = randoms.get_random(randoms.ads_weight);
+		document.write(randoms.ads_codes[rand]);
+
+	}
+}
+randoms.init();
+</script>
